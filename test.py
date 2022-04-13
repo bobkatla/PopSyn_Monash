a6980dc2a60f001411d5944844ee095051a0b23d
Duc La <Duc.La@monash.edu>
2022-04-13 14:56:12 +1000

    REFACTOR: put the sampling away con clean some simple

--- test.py
+++ test.py
@@ -8,8 +8,12 @@ import itertools
 
 ATTRIBUTES = ['AGEGROUP', 'PERSINC', 'SEX', 'CARLICENCE']
 
-# This assume that both df have the same collumns/attributes
 def SRMSE(actual, pred, attributes):
+    '''
+    This calculate the SRMSE for 2 pandas.dataframe based on the list of their attributes
+    This assumes that both df have the same collumns/attributes
+    '''
+
     total_att = 1
     full_list = {}
 
@@ -31,8 +35,8 @@ def SRMSE(actual, pred, attributes):
         check_pred = None
         for att in attributes:
             if check_actual is not None:
-                check_actual = check_actual & (actual[att] == instance[att])
-                check_pred = check_pred & (pred[att] == instance[att])
+                check_actual &= (actual[att] == instance[att])
+                check_pred &= (pred[att] == instance[att])
             else:
                 check_actual = (actual[att] == instance[att])
                 check_pred = (pred[att] == instance[att])
@@ -45,12 +49,46 @@ def SRMSE(actual, pred, attributes):
 
     return result
 
+def sampling(model, n=1000, type='forward', init_state=None):
+    '''
+    This will define different sampling methods using the BN_network (model)
+    The network is pgmpy.model.BayesianNetwork
+    '''
+    sampling_df = None
+
+    if type == 'gibbs':
+        # Using GibbsSampling, noted here it initial/seed is random by default
+        gibbs_chain = GibbsSampling(model)
+        sampling_df = gibbs_chain.sample(size=n, start_state=init_state)
+
+        # This is needed as the result of Gibb is set to only int
+        sampling_df = sampling_df.astype('object')
+        # This part is updating the cardinality of Gibb with their corresponding label
+        for att in gibbs_chain.variables:
+            pos_ref = model.get_cpds(att).state_names[att]
+            for i in sampling_df.index:
+                sampling_df.at[i, att] = pos_ref[sampling_df.at[i, att]]
+    else:
+        # Using other sampling methods
+        infer_model = BayesianModelSampling(model)
+        match type:
+            case 'forward':
+                sampling_df = infer_model.forward_sample(size=n)
+            case 'rejection':
+                sampling_df = infer_model.rejection_sample(size=n)
+            case 'likelihood':
+                sampling_df = infer_model.likelihood_weighted_sample(size=n)
+            case _:
+                print("Wrong type name")
+        
+    return sampling_df
+
 if __name__ == "__main__":
     # import data
     original_df = pd.read_csv("./data/VISTA_2012_16_v1_SA1_CSV/P_VISTA12_16_SA1_V1.csv")
     df = original_df[ATTRIBUTES].dropna()
     # It is noted that with small samples, cannot ebtablish the edges
-    seed_df = df.sample(n = 40000).copy()
+    seed_df = df.sample(n = 1000).copy()
     # print(df.shape)
     
     # Learn the DAG in data using Bayesian structure learning:
@@ -68,28 +106,9 @@ if __name__ == "__main__":
     model = bn.parameter_learning.fit(DAG, seed_df, methodtype='bayes', verbose=0)
     # bn.print_CPD(model)
 
-    # Using other sampling methods
-    infer_model = BayesianModelSampling(model['model'])
-    # sampling_df = infer_model.forward_sample(size=100)
-    # sampling_df = infer_model.likelihood_weighted_sample(size=100)
-    # sampling_df = infer_model.rejection_sample(size=10)
-
-    # Using GibbsSampling, noted here it initial/seed is randon
-    gibbs_chain = GibbsSampling(model['model'])
-    sampling_df = gibbs_chain.sample(size=100000, start_state=None)
-    # This is needed as the result of Gibb is set to only int
-    sampling_df = sampling_df.astype('object')
-    # This part is updating the cardinality of Gibb with their corresponding label
-    for att in ATTRIBUTES:
-        try:
-            pos_ref = model['model'].get_cpds(att).state_names[att]
-            for i in sampling_df.index:
-                old_val = sampling_df.at[i, att]
-                sampling_df.at[i, att] = pos_ref[old_val]
-        except:
-            continue
+    sampling_df = sampling(model['model'], n = 10, type = 'gibbs')
+    print(sampling_df)
     
     # TODO: for the missing att (they are not in the graph) they can be sampled from distribution
-    # print(sampling_df)
 
-    print(SRMSE(df, sampling_df, ATTRIBUTES))
+    # print(SRMSE(df, sampling_df, ATTRIBUTES))
