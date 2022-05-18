f6b40f50346be5829285c9abe072a2d19433c9b0
Duc Minh La <duc.la@monash.edu>
2022-05-18 10:15:11 +1000

    Refactor the code and prepare for SRMSE

--- IPF.py
+++ IPF.py
@@ -1,6 +1,52 @@
 from random import sample
 import synthpop.ipf.ipf as ipf
 import pandas as pd
+from checker import SRMSE
+
+def IPF_sampling(constraints):
+    # constraints.to_csv('./Joint_dist_result_IPF.csv')
+    return constraints
+
+
+def IPF_training(df, sample_rate):
+    atts = df.columns
+    ls_tups = []
+    margi_val = []
+    
+    for att in atts:
+        counts = df[att].value_counts()
+        indexs = list(counts.index)
+        for i, c in enumerate(counts):
+            ls_tups.append((att, indexs[i]))
+            margi_val.append(c)
+
+    # Margi dist for IPF
+    marginal_midx = pd.MultiIndex.from_tuples(ls_tups)
+    marginals = pd.Series(margi_val, index=marginal_midx)
+
+    # joint dist for IPF but only the bone
+    j_cou = df.value_counts()
+    j_idx = list(j_cou.index)
+    # To solve zero cell by making a extremely small number
+    j_vals = [0.000000000000000000000000000001]*len(j_idx)
+
+    # Fill up the vals for joint from sample
+    N = df.shape[0]
+    one_percent = int(N/100)
+    seed_df = df.sample(n = sample_rate * one_percent).copy()
+
+    seed_cou = seed_df.value_counts()
+    seed_idx = list(seed_cou.index)
+    for idx in seed_idx:
+        i = j_idx.index(idx)
+        j_vals[i] = seed_cou[idx]
+
+    joint_dist_midx = pd.MultiIndex.from_tuples(j_idx, names=atts)
+    joint_dist = pd.Series(j_vals, index=joint_dist_midx)
+    
+    constraints, iterations = ipf.calculate_constraints(marginals, joint_dist, tolerance=1e-9)
+    return IPF_sampling(constraints)
+    # print(iterations)
 
 
 if __name__ == "__main__":
@@ -23,41 +69,6 @@ if __name__ == "__main__":
         df.loc[df['CARLICENCE'] == 'No Car Licence', 'CARLICENCE'] = 'NO'
         df.loc[df['CARLICENCE'] != 'NO', 'CARLICENCE'] = 'YES'
 
-    ls_tups = []
-    margi_val = []
-    
-    for att in ATTRIBUTES:
-        counts = df[att].value_counts()
-        indexs = list(counts.index)
-        for i, c in enumerate(counts):
-            ls_tups.append((att, indexs[i]))
-            margi_val.append(c)
-
-    # Margi dist for IPF
-    marginal_midx = pd.MultiIndex.from_tuples(ls_tups)
-    marginals = pd.Series(margi_val, index=marginal_midx)
-
-    # joint dist for IPF but only the bone
-    j_cou = df.value_counts()
-    j_idx = list(j_cou.index)
-    # To solve zero cell by making a extremely small number
-    j_vals = [0.000000000000000000000000000001]*len(j_idx)
-
-    # Fill up the vals for joint from sample
-    sample_rate = 1
-    N = df.shape[0]
-    one_percent = int(N/100)
-    seed_df = df.sample(n = sample_rate * one_percent).copy()
-
-    seed_cou = seed_df.value_counts()
-    seed_idx = list(seed_cou.index)
-    for idx in seed_idx:
-        i = j_idx.index(idx)
-        j_vals[i] = seed_cou[idx]
-
-    joint_dist_midx = pd.MultiIndex.from_tuples(j_idx, names=ATTRIBUTES)
-    joint_dist = pd.Series(j_vals, index=joint_dist_midx)
-    
-    constraints, iterations = ipf.calculate_constraints(marginals, joint_dist, tolerance=1e-9)
-    constraints.to_csv('./Joint_dist_result_IPF.csv')
-    print(iterations)
+    result_sample = IPF_training(df, 10)
+    # print(SRMSE(df, result_sample))
+    print(result_sample)
