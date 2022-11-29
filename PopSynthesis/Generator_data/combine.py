9d50a25de5598b2f1e87204b21678dbb5d0fb86c
Duc Minh La <dminh.bob.la@gmail.com>
2022-11-29 14:02:00 +1100

    minor changes for sorting and better post processing

--- PopSynthesis/Generator_data/combine.py
+++ PopSynthesis/Generator_data/combine.py
@@ -29,22 +29,26 @@ def flatten_data_self_generate(ls_atts, make_like_paper=False, to_csv=False, csv
 
 def generate_control_files_from_population(pop_df, to_csv=False, csv_name_control='flat_tot.csv', csv_name_marg='flat_marg.csv'):
     # NOTE: this is mainly use in case we have a whole population data, or testing using the seed data
-    d_marg = {}
+    d_marg = {
+        'total': len(pop_df)
+    }
     d_con = {
         'seed_table': [],
         'att': [],
+        'state': [],
         'tot_name': [],
         'expression': [],
     }
     for att in pop_df.columns:
         data_seri = pop_df[att]
-        att_freq = data_seri.value_counts()
+        att_freq = data_seri.value_counts(sort=False).sort_index()
         for val in att_freq.index: 
             tot_name = f'{att}__{val}'
             d_marg[tot_name] = att_freq[val]
             
             d_con['seed_table'].append('flat_table')
             d_con['att'].append(att)
+            d_con['state'].append(val)
             d_con['tot_name'].append(tot_name)
             d_con['expression'].append(f"flat_table.{att} == '{val}'")
     df_marg = pd.DataFrame(data=d_marg, index=[0])
