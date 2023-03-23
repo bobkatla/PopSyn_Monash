b57854b5333ca83d97efb15f79030fbee7330290
Duc Minh La <dminh.bob.la@gmail.com>
2023-03-23 17:44:21 +1100

    changes to include the state as well, quick refactoring

--- PopSynthesis/Generator_data/create_LGA_PopSim.py
+++ PopSynthesis/Generator_data/create_LGA_PopSim.py
@@ -18,7 +18,7 @@ LS_ATTS_PP = [
 ]
 
 
-def get_census_popsim(ls_atts):
+def get_census_popsim(ls_atts, ls_zones=[]):
     NotImplemented
 
 
@@ -35,14 +35,22 @@ def simple_get_geo(ls_zones, state_num=2):
     NotImplemented
 
 
+def get_state_agg(df, state_nume=2):
+    # This will create a df that simply have the State as total/ aggregated of the given df
+    NotImplemented
+
+
 if __name__ == "__main__":
     seed_hh_raw = get_hh_seed(LS_ATTS_HH)
     seed_pp_raw = get_pp_seed(LS_ATTS_PP)
-    census_LGA = get_census_popsim(LS_ATTS_CENSUS)
-    geo_cross = simple_get_geo([])
 
-    seed_hh_raw.to_csv("")
-    seed_pp_raw.to_csv("")
-    census_LGA.to_csv("")
-    geo_cross.to_csv("")
-    
+    ls_zones = None
+    census_LGA = get_census_popsim(LS_ATTS_CENSUS, ls_zones=ls_zones)
+    geo_cross = simple_get_geo(ls_zones=ls_zones)
+    census_state = get_state_agg(census_LGA)
+
+    seed_hh_raw.to_csv("hh_seed.csv")
+    seed_pp_raw.to_csv("pp_seed.csv")
+    census_LGA.to_csv("LGA_controls.csv")
+    geo_cross.to_csv("geo_cross_walk.csv")
+    census_state.to_csv("state_controls.csv")
