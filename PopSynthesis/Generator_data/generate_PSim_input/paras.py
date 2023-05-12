f5fb893b33e5b55f19495b6b6e85f89c0373c033
Duc Minh La <dminh.bob.la@gmail.com>
2023-05-12 13:15:36 +1000

    organise better with paras.py

---
+++ PopSynthesis/Generator_data/generate_PSim_input/paras.py
@@ -0,0 +1,14 @@
+seed_atts_P =[
+
+] 
+seed_atts_H = [
+
+]
+census_atts = [
+
+]
+
+# Assuming all sample/VISTA data are in the same folder
+loc_file_vista = ""
+# Assuming all census data pack are in the same place, this is the .gpkg file
+loc_file_census = ""
