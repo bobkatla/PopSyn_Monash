db4a101ebacf8be609aab2018fc095dae500c508
Duc Minh La <dminh.bob.la@gmail.com>
2023-05-12 17:31:36 +1000

    finish with basic importing of the census data, basic strucs laid for sample data

--- PopSynthesis/Generator_data/generate_PSim_input/create_PopSim_input.py
+++ PopSynthesis/Generator_data/generate_PSim_input/create_PopSim_input.py
@@ -1,16 +1,39 @@
 import pandas as pd
+import geopandas as gpd
+from paras import seed_atts_H, seed_atts_P, census_atts, loc_file_census, loc_file_vista
 
 
-def get_seed_P(atts):
-    NotImplemented
+def get_seed_P(atts, dict_new_id):
+    df = pd.read_csv(f"{loc_file_vista}P_VISTA_1220_SA1.csv")
+    df = df[atts]
+    df['hh_num'] = df['hhid'].map(dict_new_id)
+    # an extra step to filter out persons corresponding with the available households only
+    df = df[df['hh_num'].notnull()]
+    return df
 
 
 def get_seed_H(atts):
-    NotImplemented
+    df = pd.read_csv(f"{loc_file_vista}H_VISTA_1220_SA1.csv")
+    # Process the VISTA to match with LG
+    df = df[atts]
+    df = df[df['wdhhwgt_sa3'].notnull()]
+    df['hh_num'] = df.index
+    return df
 
 
 def get_census_sa(atts, sa_level):
-    NotImplemented
+    inside_atts = atts.copy()
+    inside_atts.extend([
+        f"{sa_level}_CODE_2021",
+        f"{sa_level}_NAME_2021"
+    ])
+    gdf_p = gpd.read_file(f"{loc_file_census}G01_VIC_GDA2020.gpkg", layer=f"G01_{sa_level}_2021_VIC") # sex and agegroup
+    gdf_h = gpd.read_file(f"{loc_file_census}G34_VIC_GDA2020.gpkg", layer=f"G34_{sa_level}_2021_VIC") # number veh
+    # NOTE: combine later with other tables, now just this
+    final_gdf = gdf_h.merge(gdf_p, how='inner')
+    # The step with ls_zones to have only the zones in VISTA (greater Mel and geelong)
+    df = pd.DataFrame(final_gdf)
+    return df[inside_atts]
 
 
 def get_geo_cross():
@@ -18,7 +41,6 @@ def get_geo_cross():
 
 
 def get_ls_needed_df(seed_atts_P, seed_atts_H, census_atts):
-
     seed_data_P = get_seed_P(seed_atts_P)
     seed_data_H = get_seed_H(seed_atts_H)
     census_data_sa1 = get_census_sa(census_atts, sa_level="SA1")
@@ -26,6 +48,9 @@ def get_ls_needed_df(seed_atts_P, seed_atts_H, census_atts):
     census_data_sa3 = get_census_sa(census_atts, sa_level="SA3")
     census_data_sa4 = get_census_sa(census_atts, sa_level="SA4")
     geo_cross = get_geo_cross()
+
+    # Printing test
+    print(census_data_sa1)
 
     return (
         (seed_data_P, "P_sample.csv"),
@@ -43,16 +68,6 @@ def output_csv(ls_to_csv, out_loc="./"):
 
 
 def main():
-    seed_atts_P =[
-
-    ] 
-    seed_atts_H = [
-
-    ]
-    census_atts = [
-
-    ]
-
     ls_to_csv = get_ls_needed_df(seed_atts_P, seed_atts_H, census_atts)
 
     output_csv(ls_to_csv, out_loc="./")
--- PopSynthesis/Generator_data/generate_PSim_input/paras.py
+++ PopSynthesis/Generator_data/generate_PSim_input/paras.py
@@ -1,14 +1,32 @@
 seed_atts_P =[
-
+    'hhid',
+    'persid',
+    "sex",
+    "persinc",
+    "wdperswgt_sa3"
 ] 
 seed_atts_H = [
-
+    'hhid',
+    'homesa1',
+    'totalvehs',
+    'hhsize',
+    'wdhhwgt_sa3'
 ]
 census_atts = [
-
+    "Tot_P_P",
+    "Tot_P_M",
+    "Tot_P_F",
+    'Num_MVs_per_dweling_0_MVs',
+    'Num_MVs_per_dweling_1_MVs',
+    'Num_MVs_per_dweling_2_MVs',
+    'Num_MVs_per_dweling_3_MVs',
+    'Num_MVs_per_dweling_4mo_MVs',
+    'Num_MVs_per_dweling_Tot',
+    'Num_MVs_NS',
+    'Total_dwelings',
 ]
 
 # Assuming all sample/VISTA data are in the same folder
-loc_file_vista = ""
+loc_file_vista = "../data/source2/VISTA/"
 # Assuming all census data pack are in the same place, this is the .gpkg file
-loc_file_census = ""
+loc_file_census = "../data/source2/CENSUS/"
