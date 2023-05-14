1b0bd21691af6729fd859834a8b10b07ca94c73d
Duc Minh La <dminh.bob.la@gmail.com>
2023-05-14 20:58:38 +1000

    minor changes to test better with SA

--- PopSynthesis/Generator_data/generate_PSim_input/checking.py
+++ PopSynthesis/Generator_data/generate_PSim_input/checking.py
@@ -2,6 +2,7 @@
 File to double check after we got simple data
 Mostly will the case of zero-cell (data exists in the census but not in sample)
 --> we can aggregate or remove --> aggregate, maybe moveup 
+Also we have the issue that in the sample, some code (sa1 and sa2) are old codes, just maybe has new code for 2021 now
 
 Something todo, defo:
 1. Taking only the Metro Mel (Greater Mel and Greater Geelong), they do exist all in VISTA
--- PopSynthesis/Generator_data/vista_count.py
+++ PopSynthesis/Generator_data/vista_count.py
@@ -8,23 +8,24 @@ if __name__ == "__main__":
 	# a = fiona.listlayers('G01_VIC_GDA2020.gpkg')
 	# print(a)
 	# ['G01_GCCSA_2021_VIC', 'G01_UCL_2021_VIC', 'G01_SUA_2021_VIC', 'G01_SOSR_2021_VIC', 'G01_SOS_2021_VIC', 'G01_CED_2021_VIC', 'G01_LGA_2021_VIC', 'G01_POA_2021_VIC', 'G01_SA1_2021_VIC', 'G01_SA2_2021_VIC', 'G01_SA3_2021_VIC', 'G01_SA4_2021_VIC', 'G01_SAL_2021_VIC', 'G01_SED_2021_VIC', 'G01_STE_2021_VIC']
-	gdf = gpd.read_file("./data/source2/CENSUS/G01_VIC_GDA2020.gpkg", layer="G01_SA4_2021_VIC")
+	gdf = gpd.read_file("./data/source2/CENSUS/G01_VIC_GDA2020.gpkg", layer="G01_SA2_2021_VIC")
 	df = pd.read_csv("./data/source2/VISTA/SA/H_VISTA_1220_SA1.csv")
-	df["homesa4"] = df["homesa4"].astype("int")
-	gdf["SA4_CODE_2021"] = gdf["SA4_CODE_2021"].astype("int")
+	df["homesa2"] = df["homesa2"].astype("float")
+	gdf["SA2_CODE_2021"] = gdf["SA2_CODE_2021"].astype("float")
 	
     # Process the VISTA to match with LG
 	# df['homeLGA'] = df['homeLGA'].str.replace(r"\(.*\)","", regex=True).str.replace(" ", "")
 	# gdf['LGA_NAME_2021'] = gdf['LGA_NAME_2021'].str.replace(r"\(.*\)","", regex=True).str.replace(" ", "")
-	count_vista = dict(df['homesa4'].value_counts())
+	count_vista = dict(df['homesa2'].value_counts())
 
 	# I checked: all of the value in VISTA match with the one in CENSUS
-	# t = list(gdf['SA4_CODE_2021'])
-	# for a in count_vista:
-	# 	if a not in t:
-	# 		print(a)
+	t = list(gdf['SA2_CODE_2021'])
+	for a in count_vista:
+		if a not in t:
+			print(a)
 
-	gdf["VISTA_count"] = gdf['SA4_CODE_2021'].map(count_vista, na_action='ignore').fillna(0)
+	# gdf["VISTA_count"] = gdf['SA2_CODE_2021'].map(count_vista, na_action='ignore').fillna(0)
+	gdf["VISTA_count"] = gdf['SA2_CODE_2021'].map(count_vista, na_action='ignore')
 	# gdf.plot()
 	gdf.plot(column='VISTA_count', cmap='OrRd', edgecolor='k', legend=True)
 	plt.show()
