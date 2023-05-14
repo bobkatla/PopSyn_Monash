60ab02d75e8dc506b63150e18475f7e26bf61ed5
Duc Minh La <dminh.bob.la@gmail.com>
2023-05-14 16:20:11 +1000

    future note

---
+++ PopSynthesis/Generator_data/generate_PSim_input/checking.py
@@ -0,0 +1,10 @@
+"""
+File to double check after we got simple data
+Mostly will the case of zero-cell (data exists in the census but not in sample)
+--> we can aggregate or remove --> aggregate, maybe moveup 
+
+Something todo, defo:
+1. Taking only the Metro Mel (Greater Mel and Greater Geelong), they do exist all in VISTA
+2. Check what SA don't exist in sample, alot sa1 yes, but maybe not sa2
+3. Deal with zero cell (aggre how, maybe just combine with nearest SA)
+"""
