e0bbc4af50115cd2b31c21cd2ebe9c93e374bbd6
Duc Minh La <dminh.bob.la@gmail.com>
2023-05-17 13:37:47 +1000

    Init set up for the new idea of BN with rejection

---
+++ PopSynthesis/Methods/BN/rejectBN/main.py
@@ -0,0 +1,27 @@
+"""
+Implementing the idea of rejection BN and connect with DL
+"""
+
+import pandas as pd
+
+
+def main():
+    print("Hello")
+    # Import data
+    # Learn BN for HH
+    # Learn BN for P
+    # NOTE: SAVE THE BN
+    # Loop through each zone (SA1)
+    # Integerization using weights
+    # Rejection sampling loop for HH
+    # Rejection sampling loop for P
+    # NOTE: SAVE THE POOLS
+    # Clustering the P
+    # DL training with H atts to cluster for selection 
+    # Loop through each zone (SA1), should be from largest to smallest or entropy balance maybe (like PopSim)
+    # Loop through each HH and use the DL model for selection (start with random but maybe with constrains later)
+    # When if the cluster exhaust, sample again or I use another constrain to limit the selection, or second best 
+
+
+if __name__ == "__main__":
+    main()
