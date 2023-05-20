754e366e38bd8d95e0eb331ede2c370241dd99c9
Duc Minh La <dminh.bob.la@gmail.com>
2023-05-20 17:21:51 +1000

    minors

--- PopSynthesis/Methods/IPF/main.py
+++ PopSynthesis/Methods/IPF/main.py
@@ -1,10 +1,14 @@
 """
 The main file to do the work of generating using IPF-based
 """
+from paras import loc_data, loc_controls, loc_output
 
 
 def main():
-    print("Yes")
+    # Import and process files
+    # Run IPF
+    # Got the results
+    NotImplemented
 
 
 if __name__ == "__main__":
---
+++ PopSynthesis/Methods/IPF/paras.py
@@ -0,0 +1,7 @@
+"""
+Store all paras for running this IPF-based
+"""
+
+loc_data = "./data/"
+loc_controls = "./controls/"
+loc_output = "./output/"
