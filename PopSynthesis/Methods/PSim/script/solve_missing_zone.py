8cdffded4b3b338e56eae09df249891d7a9becee
Duc Minh La <dminh.bob.la@gmail.com>
2023-08-12 13:59:06 +1000

    ADD to get the missing zones

--- PopSynthesis/Methods/PSim/script/solve_missing_zone.py
+++ PopSynthesis/Methods/PSim/script/solve_missing_zone.py
@@ -22,28 +22,37 @@ def _sampling_BN(BN, ls_zone, n):
     
 
 def _extract_missing_zones(ls_available:list[str], ls_zones:list[str], zone_lev:str):
+    # Assuming they are the same type
+    assert ls_available.dtype == ls_zones.dtype
+    ls_available = list(ls_available)
+    ls_zones = list(ls_zones)
     # Should return a list of missing zones
-    print(ls_available, ls_zones)
-    NotImplemented
+    ls_unavai = []
+    for zone in ls_available:
+        if zone not in ls_zones:
+            ls_unavai.append(zone)
+    assert len(ls_available) == len(ls_unavai) + len(ls_zones)
+    return ls_unavai
+
 
 def main():
     # Import data
     df_seed_H = pd.read_csv("../data/H_sample.csv")
-    df_seed_P = pd.read_csv("../data/p_sample.csv")
+    # df_seed_P = pd.read_csv("../data/p_sample.csv")
 
     name_zone_lev = "SA1"
     df_census = pd.read_csv(f"../data/census_{name_zone_lev}.csv")
-    ls_all_zones = df_census[name_zone_lev]
+    ls_all_zones = df_census[name_zone_lev].astype("Int64").unique()
     # Extract missing zones
 
-    for df_seed in [df_seed_H, df_seed_P]:
-        ls_zones = df_seed[name_zone_lev]
-        ls_missing_zones = _extract_missing_zones(ls_available=ls_all_zones, ls_zones=ls_zones ,zone_lev=name_zone_lev)
-        # Learn BN
-        BN = _learn_BN(df_seed=df_seed)
-        dummy_seed = _sampling_BN(BN, ls_zone=ls_missing_zones, ratio_dummy=0.2)
-        # Combine dummy seed and original seed
-        # Output the new seed, this will be the new input
+    df_seed = df_seed_H
+    ls_zones = df_seed[name_zone_lev].astype("Int64").unique()
+    ls_missing_zones = _extract_missing_zones(ls_available=ls_all_zones, ls_zones=ls_zones ,zone_lev=name_zone_lev)
+    # Learn BN
+    BN = _learn_BN(df_seed=df_seed)
+    dummy_seed = _sampling_BN(BN, ls_zone=ls_missing_zones, ratio_dummy=0.2)
+    # Combine dummy seed and original seed
+    # Output the new seed, this will be the new input
     
 
 if __name__ == "__main__":
