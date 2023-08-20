f34dc8d4c0182b6bd61e3f23f9d4f89ae79e62da
Duc Minh La <dminh.bob.la@gmail.com>
2023-08-21 09:48:33 +1000

    simpe change

--- PopSynthesis/Benchmark/CompareFullPop/compare.py
+++ PopSynthesis/Benchmark/CompareFullPop/compare.py
@@ -3,6 +3,12 @@ This is used when we have the full population (not possible in real life) and co
 This should be more correct but not realistic/ realiable
 Often we just use the seed data as the full pop
 """
+from PopSynthesis.Benchmark.legacy.checker import update_SRMSE
+import pandas as pd
+
+
+def full_pop_SRMSE(actual: pd.DataFrame, pred: pd.DataFrame) -> float:
+    return update_SRMSE(actual=actual, pred=pred)
 
 
 def main():
