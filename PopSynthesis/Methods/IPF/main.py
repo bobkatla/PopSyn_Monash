c363524ce531492a1da451294a60a5265ba98c39
Duc Minh La <dminh.bob.la@gmail.com>
2023-08-21 13:04:06 +1000

    simple refactor

--- PopSynthesis/Methods/IPF/main.py
+++ PopSynthesis/Methods/IPF/main.py
@@ -7,10 +7,10 @@ import numpy as np
 
 
 def main():
-    min_rate, max_rate, tot = 0.01, 0.1, 10
+    min_rate, max_rate, tot = 0.9, 1, 2
     results = eval_based_on_full_pop(loc_data=loc_data, range_sample=np.linspace(min_rate, max_rate, tot))
     data = np.asarray(results)
-    np.save(f'result_{min_rate}_{max_rate}.npy', data)
+    np.save(f'./output/result_IPF_{min_rate}_{max_rate}.npy', data)
     print(results)
 
 
