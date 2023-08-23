21cee68ae1d00a5fc49d80fa3c78148eae175a13
Duc Minh La <dminh.bob.la@gmail.com>
2023-08-23 12:24:12 +1000

    change the formula for census check

--- PopSynthesis/Benchmark/CompareCensus/compare.py
+++ PopSynthesis/Benchmark/CompareCensus/compare.py
@@ -7,10 +7,13 @@ import math
 
 
 def compare_RMS_census(marg_census:pd.Series, marg_syn:pd.Series):
-    assert marg_census.index.names == marg_syn.index.names
+    ls_census_atts = list(marg_census.index.get_level_values(0).unique())
+    ls_syn_atts = list(marg_syn.index.get_level_values(0).unique())
+    assert ls_census_atts == ls_syn_atts
+    n = marg_census[ls_census_atts[0]].sum()
     hold = 0
     for k in marg_census.index:
         census_val = marg_census[k]
         syn_val = marg_syn[k] if k in marg_syn.index else 0
-        hold += (census_val - syn_val)**2
+        hold += ((census_val - syn_val)/n)**2
     return math.sqrt(hold)
