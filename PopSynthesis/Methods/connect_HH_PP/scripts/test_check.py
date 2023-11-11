0bd19cdeb997ad5dbb367e3e90954443cc647d70
Duc Minh La <dminh.bob.la@gmail.com>
2023-11-12 05:14:48 +1100

    testing

--- PopSynthesis/Methods/connect_HH_PP/scripts/test_check.py
+++ PopSynthesis/Methods/connect_HH_PP/scripts/test_check.py
@@ -1,15 +1,5 @@
-from multiprocessing import Process, Queue, , Lock
+from multiprocessing import Process, Queue, Lock
 import os
-
-
-def test(infer_model, rela):
-    print(f"Doing {rela}")
-    evidences = [State(var='age_main', state=96), State(var='sex_main', state='F'), State(var='persinc_main', state='$400-599 p.w.'), State(var='nolicence_main', state='Some Licence'), State(var='anywork_main', state='N')]
-    syn = infer_model.rejection_sample(evidence=evidences, size=2, show_progress=True)
-    remove_cols = [x for x in syn.columns if "_main" in x]
-    syn = syn.drop(columns=remove_cols)
-    print(syn)
-
 
 def info(title):
     print(title)
@@ -17,10 +7,8 @@ def info(title):
     print('parent process:', os.getppid())
     print('process id:', os.getpid())
 
-
 def f(q):
     q.put([42, None, 'hello'])
-
 
 def multi_process():
     q = Queue()
