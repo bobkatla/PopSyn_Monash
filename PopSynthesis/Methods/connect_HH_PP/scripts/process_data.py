49a08a4217538ae2339075592688aaeb381a3655
Duc Minh La <dminh.bob.la@gmail.com>
2023-11-12 05:31:26 +1100

    convert age as well to dicretize

--- PopSynthesis/Methods/connect_HH_PP/scripts/process_data.py
+++ PopSynthesis/Methods/connect_HH_PP/scripts/process_data.py
@@ -218,6 +218,28 @@ def test():
     print(pp_df)
 
 
+def convert_pp_age_gr(pp_df, range_age, age_limit):
+    check_dict = {}
+    hold_min = None
+    new_name = None
+    for i in range(age_limit):
+        if i % range_age == 0:
+            hold_min = i
+            new_name = f"{hold_min}-{hold_min+range_age-1}"
+        check_dict[i] = new_name
+    check_dict["others"] = f"{age_limit}+"
+    
+    def convert_age(row):
+        if row["age"] in check_dict:
+            return check_dict[row["age"]]
+        else:
+            return check_dict["others"]
+        
+    pp_df["age"] = pp_df.apply(convert_age, axis=1)
+    return pp_df
+
+
+
 def main():
     # Import HH and PP samples (VISTA)
     hh_df_raw = pd.read_csv("..\..\..\Generator_data\data\source2\VISTA\SA\H_VISTA_1220_SA1.csv")
