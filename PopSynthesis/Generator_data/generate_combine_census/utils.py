7a42735f4546d6d8ec698d1bf0b3a8ee0abc699a
Duc Minh La <dminh.bob.la@gmail.com>
2023-11-28 00:46:04 +1100

    adding some utils to process data

--- PopSynthesis/Generator_data/generate_combine_census/utils.py
+++ PopSynthesis/Generator_data/generate_combine_census/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import glob
+import numpy as np
 
 
 data_loc = "../data/tablebuilder"
@@ -39,6 +40,54 @@ def process_from_census_data(geo_lev='POA'):
     final_df = pd.concat(ls_df, axis=1)
     final_df = final_df.dropna(axis=0, thresh=10)
     return final_df
+    
+
+def TRS(initial_weights, know_tot=None):
+    # Desired total constraint
+    desired_total = know_tot if know_tot else sum(initial_weights)
+
+    # Step 1: Truncate the weights to integers
+    truncated_weights = np.floor(initial_weights).astype(int)
+
+    # Step 2: Calculate the discrepancy
+    total_truncated = np.sum(truncated_weights)
+    discrepancy = desired_total - total_truncated
+
+    # Step 3: Replicate individuals to match the constraint
+    if discrepancy > 0:
+        # Calculate fractional parts
+        fractional_parts = initial_weights - truncated_weights
+        
+        # Replicate individuals in proportion to their fractional parts
+        replication_probs = fractional_parts / np.sum(fractional_parts)
+        num_replications = np.random.multinomial(int(discrepancy), replication_probs)
+        truncated_weights += num_replications
+
+    # Step 4: Sample individuals if there is an excess
+    if discrepancy < 0:
+        excess_indices = np.where(truncated_weights > 0)[0]
+        excess_weights = truncated_weights[excess_indices]
+        
+        # Calculate sampling probabilities based on truncated weights
+        sampling_probs = excess_weights / np.sum(excess_weights)
+        
+        # Randomly sample individuals to reduce excess
+        num_samples = np.random.multinomial(abs(discrepancy), sampling_probs)
+        truncated_weights[excess_indices] -= num_samples
+
+    # Step 5: Your final truncated and rounded integer weights
+    return truncated_weights
+
+
+def update_int_all(sub_df, total_seri):
+    cols = sub_df.columns
+    def f(r):
+        vals = list(r)
+        poa = r.index
+        tot = total_seri[poa]
+        return TRS(vals, tot)
+    sub_df[cols] = sub_df.apply(f, axis=1, result_type="expand")
+    return sub_df
 
 
 def main():
