1bea5dbcdad6ba4c0e86cbed3fbfbc1648bd9391
Duc Minh La <dminh.bob.la@gmail.com>
2023-11-28 14:20:32 +1100

    running ipu fine

--- PopSynthesis/Methods/IPU/scripts/test.py
+++ PopSynthesis/Methods/IPU/scripts/test.py
@@ -12,9 +12,6 @@ def test_run(hh_marg, p_marg, hh_sample, p_sample):
                                                                p_marg,
                                                                hh_sample,
                                                                p_sample)
-    print(hh_marg)
-    print(p_marg)
-    print(xwalk)
     # a = zs.synch_hhids(hh_marg, p_marg, xwalk)
     # sample_df = hh_sample[hh_sample.sample_geog == xwalk[0][1]]
     # category_df = cat.category_combinations(hh_marg.columns)
@@ -28,29 +25,32 @@ def test_run(hh_marg, p_marg, hh_sample, p_sample):
     # print(hhs)
     # print(hh_jd)
 
-    # all_households, all_persons, all_stats = zs.synthesize_all_zones(hh_marg,
-    #                                                                  p_marg,
-    #                                                                  hh_sample,
-    #                                                                  p_sample,
-    #                                                                  xwalk)
+    all_households, all_persons, all_stats = zs.synthesize_all_zones(hh_marg,
+                                                                     p_marg,
+                                                                     hh_sample,
+                                                                     p_sample,
+                                                                     xwalk)
     
-    # print(all_households)
-    # print(all_persons)
-    # print(all_stats)
-    
+    print(all_households)
+    print(all_persons)
+    print(all_stats)
+    all_households.to_csv("../output/syn_hh_ipu.csv", index=False)
+    all_persons.to_csv("../output/syn_pp_ipu.csv", index=False)
+    all_stats.to_csv("../output/stats_ipu.csv", index=False)
+
 
 def main():
-    name_f = lambda x: os.path.join(r'C:\Users\dlaa0001\Documents\PhD\PopSyn_Monash\PopSynthesis\Methods\IPU\data\test_examples', f'{x}.csv')
-    # hh_marg = name_f("hh_marginals_ipu")
-    # p_marg = name_f("person_marginals_ipu")
-    # hh_sample = name_f("hh_sample_ipu")
-    # p_sample = name_f("pp_sample_ipu")
+    name_f = lambda x: os.path.join(r'C:\Users\dlaa0001\Documents\PhD\PopSyn_Monash\PopSynthesis\Methods\IPU\data\testing_new', f'{x}.csv')
+    hh_marg = name_f("hh_marginals_ipu")
+    p_marg = name_f("person_marginals_ipu")
+    hh_sample = name_f("hh_sample_ipu")
+    p_sample = name_f("pp_sample_ipu")
 
     # TESTING SAMPLE
-    hh_marg = name_f("hh_marginals")
-    p_marg = name_f("person_marginals")
-    hh_sample = name_f("household_sample")
-    p_sample = name_f("person_sample")
+    # hh_marg = name_f("hh_marginals")
+    # p_marg = name_f("person_marginals")
+    # hh_sample = name_f("household_sample")
+    # p_sample = name_f("person_sample")
     a = test_run(hh_marg, p_marg, hh_sample, p_sample)
 
 
