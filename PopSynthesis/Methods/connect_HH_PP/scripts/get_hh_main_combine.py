98465e5cbded5b1512f5497cbd9e9a2d181bb3fe
Duc Minh La <dminh.bob.la@gmail.com>
2024-02-06 16:18:27 +1100

    optimised the hh_main comb from given hh_df

--- PopSynthesis/Methods/connect_HH_PP/scripts/get_hh_main_combine.py
+++ PopSynthesis/Methods/connect_HH_PP/scripts/get_hh_main_combine.py
@@ -30,29 +30,69 @@ def get_combine_df(hh_df):
     # drop all the ids as they are not needed for in BN learning
     id_cols = [x for x in df_seed.columns if "hhid" in x or "persid" in x]
     df_seed = df_seed.drop(columns=id_cols)
-    pool_hh_main = get_pool(df_seed, state_names)
+    pool_hh_main = get_pool(df_seed, state_names, POOL_SZ)
     pool_hh_main = pool_hh_main.astype(str)
+    count_pool = pool_hh_main.value_counts()
+    count_pool = count_pool.reset_index()
+    count_pool["id_pool"] = count_pool.index
 
-    ls_match = list(hh_df.columns)
-    ls_match.remove("POA")
-    hh_df["hhid"] = hh_df.index
     hh_df = hh_df.astype(str)
+    ls_match = list(hh_df.columns)
+    ls_match.remove("hhid")
+    gb_df = hh_df.groupby(ls_match)["hhid"].apply(lambda x: list(x))
+    ls_match.remove("POA")
+    gb_df = gb_df.reset_index()
 
-    ls_df = []
-    for poa in hh_df["POA"].unique():
-        print(f"DOING {poa}")
-        sub_df = hh_df[hh_df["POA"]==poa]
-        combine = sub_df.merge(pool_hh_main, on=ls_match, how="left")
-        combine = combine.sample(frac=1).drop_duplicates(subset='hhid')
-        ls_df.append(combine)
+    # Concat first then explode, it will save run time
+    combine = gb_df.merge(count_pool, on=ls_match, how="left")
+    combine["hhid"] = combine["hhid"].astype(str)
+
+    c_ls_id = combine.groupby("hhid")["id_pool"].apply(lambda x: list(x))
+    c_ls_count = combine.groupby("hhid")["count"].apply(lambda x: list(x))
+    c_ls_com = pd.concat([c_ls_id, c_ls_count], axis=1)
+    df_com = c_ls_com.reset_index()
+    df_com["hhid"] = df_com["hhid"].apply(lambda x: eval(x))
+
+    def ran_sam(r):
+        ids_choose_from = r["id_pool"]
+        if str(ids_choose_from[0]) == "nan":
+            return None
+        else:
+            counts = r["count"]
+            p = [x/ sum(counts) for x in counts]
+            sz = len(r["hhid"])
+            return np.random.choice(ids_choose_from, size=sz, p=p)
+
+    df_com["selection"] = df_com.apply(ran_sam, axis=1)
+    del_df = df_com[df_com["selection"].isna()]
+    keep_df = df_com[~df_com["selection"].isna()]
+
+    # Process keeping
+    keep_df = keep_df.drop(columns=["id_pool", "count"])
+    keep_df = keep_df.explode(["hhid", "selection"])
+    keep_df = keep_df.rename(columns={"selection": "id_pool"})
+    com_df_hh = keep_df.merge(hh_df, on="hhid")
+    count_pool = count_pool.drop(columns=hh_df.columns, errors="ignore")
+    com_df_hh_main = com_df_hh.merge(count_pool, on="id_pool")
+    com_df_hh_main = com_df_hh_main.drop(columns=["id_pool", "count"])
+
+    # process del
+    del_df = del_df.explode("hhid")
+    com_df_del = del_df.merge(hh_df, on="hhid")
+    com_df_del = com_df_del.drop(columns=["id_pool", "count", "selection"])
+
+    assert len(com_df_del) + len(com_df_hh_main) == len(hh_df)
     
-    fin_df_all = pd.concat(ls_df)
-    fin_df_all.to_csv("./my_fisrt_comb3.csv", index=False)
+    return com_df_hh_main, com_df_del
 
 
 def main():
-    hh_df = pd.read_csv(r"C:\Users\dlaa0001\Documents\PhD\PopSyn_Monash\PopSynthesis\Methods\connect_HH_PP\output\adjust\saving_hh_totalvehs.csv")
-    combine_df = get_combine_df(hh_df)
+    hh_df = pd.read_csv(os.path.join(output_dir, "adjust", "final", "saving_hh_dwelltype.csv"))
+    hh_df["hhid"] = hh_df.index
+    combine_df, del_df = get_combine_df(hh_df)
+    print(combine_df)
+    print(len(hh_df))
+    print(del_df)
 
 
 if __name__ == "__main__":
