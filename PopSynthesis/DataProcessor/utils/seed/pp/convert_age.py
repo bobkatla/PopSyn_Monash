1474431337a8dc6b745fe9a7cea54019cf28bf17
Duc Minh La <dminh.bob.la@gmail.com>
2024-08-18 23:15:14 +1000

    minors update, we don't use max age anymore

--- PopSynthesis/DataProcessor/utils/seed/pp/convert_age.py
+++ PopSynthesis/DataProcessor/utils/seed/pp/convert_age.py
@@ -1,21 +1,8 @@
-def get_main_max_age(pp_df):
-    # add the dummy inc to rank
-    ls_hh_id = pp_df["hhid"].unique()
-    for hh_id in ls_hh_id:
-        print(hh_id)
-        sub_df = pp_df[pp_df["hhid"] == hh_id]
-        idx_max_age = sub_df["age"].idxmax()
-        rela_max_age = sub_df.loc[idx_max_age]["relationship"]
-        # CONFIRMED this will be Spouse or Others only
-        pp_df.at[idx_max_age, "relationship"] = "Main"
-        if rela_max_age != "Self":
-            sub_sub_df = sub_df[sub_df["relationship"] == "Self"]
-            idx_self = sub_sub_df.index[0]
-            pp_df.at[idx_self, "relationship"] = rela_max_age
-    return pp_df
+import polars as pl
 
 
 def convert_pp_age_gr(pp_df, range_age=10, age_limit=100):
+    pp_df = pp_df.to_pandas()
     check_dict = {}
     hold_min = None
     new_name = None
@@ -33,4 +20,4 @@ def convert_pp_age_gr(pp_df, range_age=10, age_limit=100):
             return check_dict["others"]
 
     pp_df["age"] = pp_df.apply(convert_age, axis=1)
-    return pp_df
+    return pl.from_pandas(pp_df)
