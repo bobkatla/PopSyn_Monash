831692eb0192c74d571c41f81bd59bb44e50d623
Duc Minh La <dminh.bob.la@gmail.com>
2024-08-21 16:02:02 +1000

    fixed and got the 1 layer samping done with polars

--- PopSynthesis/Methods/IPSF/SAA/operations.py
+++ PopSynthesis/Methods/IPSF/SAA/operations.py
@@ -1,22 +1,45 @@
 """ Contains the funcs for SAA, help seperate out and easier debugging"""
 
 import polars as pl
+import numpy as np
 
 from typing import List, Union
+from PopSynthesis.Methods.IPSF.const import count_field, zone_field
+
+
+def sample_from_pl(df: pl.DataFrame, n: int, count_field:str = count_field, with_replacement=True) -> pl.DataFrame:
+    # Normalize weights to sum to 1
+    weights = df[count_field].to_numpy()
+    weights = weights/weights.sum()
+    sample_indices = np.random.choice(df.height, size=n, replace=with_replacement, p=weights)
+    return df[sample_indices.tolist()]
 
 
 def init_syn_pop_saa(att:str, marginal_data: pl.DataFrame, pool: pl.DataFrame) -> pl.DataFrame:
-    assert "zone_id" in marginal_data
-    assert set(pool[att].unique()) == set(marginal_data.columns)
-    for state in marginal_data.columns:
+    assert zone_field in marginal_data
+    states = list(pool[att].unique())
+    assert set(states + [zone_field]) == set(marginal_data.columns)
+
+    if count_field not in pool.columns:
+        pool = pool.with_columns([pl.lit(1).alias(count_field)])
+
+    sub_pops = []
+    for state in states:
         sub_pool = pool.filter(pl.col(att) == state)
         if len(sub_pool) == 0:
             print(
                 f"WARNING: cannot see {att}_{state} in the pool, sample by the rest"
             )
             sub_pool = pool  # if there are none, we take all
-        tot_state = marginal_data[state]
-
+        for zone in marginal_data[zone_field]:
+            condition = marginal_data.filter(pl.col(zone_field) == zone)
+            census_val = condition.select(state).to_numpy()[0,0]
+            # sub_syn_pop = pool.sample(n=census_val, weights=w_sample, =True)
+            sub_syn_pop = sample_from_pl(sub_pool, census_val)
+            sub_syn_pop = sub_syn_pop.with_columns([pl.lit(zone).alias(zone_field)])
+            sub_syn_pop = sub_syn_pop.drop(count_field)
+            sub_pops.append(sub_syn_pop)
+    return pl.concat(sub_pops)
 
 
 def adjust_atts_state_match_census(att: str, curr_syn_pop: Union[None, pl.DataFrame], census_data: pl.DataFrame, adjusted_atts: List[str], pool: pl.DataFrame) -> pl.DataFrame:
--- PopSynthesis/tests/IPSF/SAA/test_operations.py
+++ PopSynthesis/tests/IPSF/SAA/test_operations.py
@@ -1,1 +1,29 @@
-fr
+from PopSynthesis.Methods.IPSF.SAA.operations import init_syn_pop_saa
+from PopSynthesis.Methods.IPSF.const import zone_field
+
+from pathlib import Path
+import polars as pl
+import pandas as pd
+
+
+data_folder = Path(__file__).parent.parent.parent.resolve() / "test_data"
+expected_folder = None
+
+hh_marg = pd.read_csv(data_folder / "hh_marg_minor_test.csv", header=[0,1])
+hh_sample = pd.read_csv(data_folder / "hh_sample_minors.csv")
+
+def test_init_syn_pop():
+    atts = [x for x in hh_sample.columns if x not in ["serialno", "sample_geog"] ]
+    segmented_marg = {}
+    zones = hh_marg[hh_marg.columns[hh_marg.columns.get_level_values(0)==zone_field]].values
+    zones = [z[0] for z in zones]
+    for att in atts:
+        sub_marg = hh_marg[hh_marg.columns[hh_marg.columns.get_level_values(0)==att]]
+        sub_marg.columns = sub_marg.columns.droplevel(0)
+        sub_marg.loc[:, [zone_field]] = zones
+        segmented_marg[att] = pl.from_pandas(sub_marg)
+    results_run = init_syn_pop_saa("hhsize", segmented_marg["hhsize"], pl.from_pandas(hh_sample))
+    for zone in zones:
+        print(segmented_marg["hhsize"].filter(pl.col(zone_field)==zone))
+        print(results_run.filter(pl.col(zone_field)==zone)["hhsize"].value_counts())
+test_init_syn_pop()
