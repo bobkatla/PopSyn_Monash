18d60472d95a5af4bb626d6242cbcfe66946ad89
Duc Minh La <dminh.bob.la@gmail.com>
2024-08-23 14:37:00 +1000

    adding new fix for req

--- PopSynthesis/Methods/IPU/scripts/run.py
+++ PopSynthesis/Methods/IPU/scripts/run.py
@@ -5,7 +5,7 @@ import time
 
 from pathlib import Path
 ipu_dir_path = Path(__file__).parent.parent.resolve()
-data_path = Path(ipu_dir_path / "data" / "testing_new")
+data_path = Path(ipu_dir_path / "data")
 output_path = Path(ipu_dir_path / "output")
 
 
@@ -13,18 +13,6 @@ def test_run(hh_marg, p_marg, hh_sample, p_sample):
     hh_marg, p_marg, hh_sample, p_sample, xwalk = zs.load_data(
         hh_marg, p_marg, hh_sample, p_sample
     )
-    # a = zs.synch_hhids(hh_marg, p_marg, xwalk)
-    # sample_df = hh_sample[hh_sample.sample_geog == xwalk[0][1]]
-    # category_df = cat.category_combinations(hh_marg.columns)
-    # hhs, hh_jd = cat.joint_distribution(
-    #         hh_sample[hh_sample.sample_geog == xwalk[0][1]],
-    #         cat.category_combinations(hh_marg.columns))
-
-    # category_names = list(category_df.index.names)
-    # print(sample_df.groupby(category_names).size())
-    # category_df["frequency"] = sample_df.groupby(category_names).size()
-    # print(hhs)
-    # print(hh_jd)
 
     all_households, all_persons, all_stats = zs.synthesize_all_zones(
         hh_marg, p_marg, hh_sample, p_sample, xwalk
@@ -45,12 +33,6 @@ def main():
     p_marg = name_f("person_marginals_ipu")
     hh_sample = name_f("hh_sample_ipu")
     p_sample = name_f("pp_sample_ipu")
-
-    # TESTING SAMPLE
-    # hh_marg = name_f("hh_marginals")
-    # p_marg = name_f("person_marginals")
-    # hh_sample = name_f("household_sample")
-    # p_sample = name_f("person_sample")
     
     start_time = time.time()
     test_run(hh_marg, p_marg, hh_sample, p_sample)
