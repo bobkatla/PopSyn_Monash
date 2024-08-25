cf6e8b094be5d12be207fd79b42194bc633d993f
Duc Minh La <dminh.bob.la@gmail.com>
2024-08-25 23:52:57 +1000

    ADD SAA: add const for files path

--- PopSynthesis/Methods/IPSF/const.py
+++ PopSynthesis/Methods/IPSF/const.py
@@ -1,5 +1,11 @@
+from pathlib import Path
+
 zone_field = "zone_id"
 hhid_field = "hhid"
 count_field = "saa_count"
 
 POOL_SIZE = 1e7
+
+data_dir = Path(__file__).resolve() / "data" / "raw"
+processed_dir = Path(__file__).resolve() / "data" / "processed"
+output_dir = Path(__file__).resolve() / "output"
