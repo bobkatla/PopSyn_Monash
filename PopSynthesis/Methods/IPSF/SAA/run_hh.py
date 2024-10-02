42fe9e2d92f031fd9a9bdadc32fd317baa046de1
Duc Minh La <dminh.bob.la@gmail.com>
2024-10-02 16:27:45 +1000

    timer

--- PopSynthesis/Methods/IPSF/SAA/run_hh.py
+++ PopSynthesis/Methods/IPSF/SAA/run_hh.py
@@ -6,6 +6,7 @@ import pickle
 from PopSynthesis.Methods.IPSF.const import data_dir, processed_dir, POOL_SIZE
 from PopSynthesis.Methods.IPSF.utils.pool_utils import create_pool
 from PopSynthesis.Methods.IPSF.SAA.main import SAA
+import time
 
 
 def run_main() -> None:
@@ -24,7 +25,17 @@ def run_main() -> None:
     pool = create_pool(seed=hh_seed, state_names=hh_att_state, pool_sz=1000000)
     hh_marg = hh_marg.head(5)
     saa = SAA(hh_marg, hh_seed, order_adjustment, hh_att_state, pool)
+
+    start_time = time.time()
+    ###
     final_syn_pop = saa.run()
+    ###
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    hours, rem = divmod(elapsed_time, 3600)  # 3600 seconds in an hour
+    minutes, seconds = divmod(rem, 60)       # 60 seconds in a minute
+    print(f"Processing took {int(hours)}h-{int(minutes)}m-{seconds:.2f}s")
+    
     print(final_syn_pop)
 
 
