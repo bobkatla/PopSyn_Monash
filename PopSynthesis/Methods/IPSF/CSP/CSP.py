0113135bd532661fbd171b35a77f5629e1fba6a7
Duc Minh La <dminh.bob.la@gmail.com>
2024-10-09 23:24:03 +1100

    setup for CSP

---
+++ PopSynthesis/Methods/IPSF/CSP/CSP.py
@@ -0,0 +1,17 @@
+"""
+Here defind the object of CSP that will take in synthetic households to generate people
+
+This will create the pools for households and people (by each pairs)
+Go through pair hh-main to get main (ensure the main people match all exist in pools)
+Then main with other rela and update the pools of main and other rela as well
+Maybe need pools for people (in general) and pools for households, we then can cross check
+we then will update the households again (cross check with census and re-run it)
+We will add the loop here as well (the longest is the fisrt SAA which is seperately)
+"""
+
+class CSP:
+    def __init__(self) -> None:
+        NotImplemented
+
+    def run():
+        NotImplemented
---
+++ PopSynthesis/Methods/IPSF/utils/synthetic_checked_census.py
@@ -0,0 +1,15 @@
+"""
+Check synthetic population with census
+
+Input will be synthetic pop and its related census
+This remove the least part we can to make it correct (smaller than census)
+This require func to convert synthetic to census form (agg func)
+Then func to detect which part we can remove
+Output will be part of synthetic we keep and part we want to remove
+"""
+
+def convert_synthetic_to_census():
+    NotImplemented
+
+def check_synthetic_against_census():
+    NotImplemented
