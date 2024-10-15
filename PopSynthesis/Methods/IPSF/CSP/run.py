cd119f0d55dad36e368c0546582f1bb3ab9686df
Duc Minh La <dminh.bob.la@gmail.com>
2024-10-15 17:33:44 +1100

    seperate for generate the pool pairs

--- PopSynthesis/Methods/IPSF/CSP/run.py
+++ PopSynthesis/Methods/IPSF/CSP/run.py
@@ -11,40 +11,8 @@ from PopSynthesis.Methods.IPSF.utils.pool_utils import create_pool
 def main():
     # TODO: is there anyway to use pp marg
     # get the data
-    hh_marg = pd.read_csv(data_dir / "hh_marginals_ipu.csv", header=[0, 1])
-    hh_seed = pd.read_csv(data_dir / "hh_sample_ipu.csv").drop(columns=["sample_geog"])
-    pp_seed = pd.read_csv(data_dir / "pp_sample_ipu.csv").drop(columns=["sample_geog"])
-    with open(processed_dir / "dict_hh_states.pickle", "rb") as handle:
-        hh_att_state = pickle.load(handle)
-    with open(processed_dir / "dict_pp_states.pickle", "rb") as handle:
-        pp_att_state = pickle.load(handle)
-
-    hh_marg = hh_marg.drop(columns=hh_marg.columns[hh_marg.columns.get_level_values(0)=="sample_geog"][0])
-    # vars
-    rela_col = "relationship"
-    id_col = "serialno"
-    main_rela = "Main"
-
-    # process seed
-    seed_pairs = convert_seeds_to_pairs(hh_seed, pp_seed, id_col, rela_col, main_rela)
-
-    # create pools
-    pools_ref = {}
-    for pair_name, pair_seed in seed_pairs.items():
-        name1, name2 = pair_name.split("-")
-        ori_states_1 = hh_att_state if name1 == "HH" else pp_att_state
-        ori_states_2 = pp_att_state # because the second one always people
-        processed_states_ref = pair_states_dict(ori_states_1, ori_states_2, name1, name2)
-        # we only need matching columns that we wish to process for BN
-        # this excludes relationship and ids
-        assert set(processed_states_ref.keys()) <= set(pair_seed.columns)
-        to_filter_col = list(processed_states_ref.keys())
-        if name1 == "HH":
-            # special case to add the rela cols
-            to_filter_col += list(pp_seed[rela_col].unique())
-        filtered_seed = pair_seed[to_filter_col]
-        pools_ref[pair_name] = create_pool(filtered_seed, state_names=processed_states_ref, pool_sz=POOL_SIZE)
-
+    with open(processed_dir / "dict_pool_pairs.pickle", "rb") as handle:
+        pools_ref = pickle.load(handle)
     print(pools_ref)
     
 
