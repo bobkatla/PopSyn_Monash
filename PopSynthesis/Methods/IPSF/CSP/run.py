8fe1b0b6a43bbb05db5e56f0ae6a803b5b28d7c5
Duc Minh La <dminh.bob.la@gmail.com>
2024-10-16 01:24:53 +1100

    added the filter to rm wrong HH synthesis

--- PopSynthesis/Methods/IPSF/CSP/run.py
+++ PopSynthesis/Methods/IPSF/CSP/run.py
@@ -3,6 +3,7 @@
 
 import pickle
 from PopSynthesis.Methods.IPSF.const import processed_dir, PP_ATTS, NOT_INCLUDED_IN_BN_LEARN
+from PopSynthesis.Methods.IPSF.CSP.operations.extra_filters import filter_mismatch_hhsz
 
 
 def main():
@@ -11,8 +12,12 @@ def main():
     with open(processed_dir / "dict_pool_pairs.pickle", "rb") as handle:
         pools_ref = pickle.load(handle)
     pp_atts = list(set(PP_ATTS) - set(NOT_INCLUDED_IN_BN_LEARN))
-    print(pp_atts)
+    all_rela = [x.split("-")[-1] for x in pools_ref.keys()]
     # rename the HH-Main so the so Main match the rest
+    rename_main = {x: f"{x}_Main" for x in pp_atts}
+    pools_ref["HH-Main"] = pools_ref["HH-Main"].rename(columns=rename_main)
+    pools_ref["HH-Main"] = filter_mismatch_hhsz(pools_ref["HH-Main"], "hhsize", all_rela)
+    print(pools_ref["HH-Main"])
 
 
 if __name__ == "__main__":
