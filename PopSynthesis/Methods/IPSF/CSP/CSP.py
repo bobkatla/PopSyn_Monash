c603c447521a7052ae3cd3895edf7596a70b6e80
Duc Minh La <dminh.bob.la@gmail.com>
2024-10-23 03:57:43 +1100

    setup for IPSF full run

--- PopSynthesis/Methods/IPSF/CSP/CSP.py
+++ PopSynthesis/Methods/IPSF/CSP/CSP.py
@@ -9,11 +9,123 @@ Maybe need pools for people (in general) and pools for households, we then can c
 we then will update the households again (cross check with census and re-run it)
 We will add the loop here as well (the longest is the fisrt SAA which is seperately)
 """
+import pandas as pd
+from PopSynthesis.Methods.IPSF.const import HH_TAG
+from PopSynthesis.Methods.IPSF.CSP.operations.sample_from_pairs import (
+    sample_matching_from_pairs,
+    create_count_col,
+)
+import numpy as np
+from typing import Literal, Dict, Tuple
+
+ordered_pairs =[
+    [
+        ("HH", "Main")
+    ],
+    [
+        ("Main", "Spouse"), 
+        ("Main", "Child"), 
+        ("Main", "Parent"),
+        ("Main", "Sibling"),
+        ("Main", "Others")
+    ], 
+    [
+        ("Child", "Grandchild"), 
+        ("Parent", "Grandparent")
+    ]
+]
+
+HHID = "hhid"
 
 
-class CSP:
-    def __init__(self) -> None:
-        NotImplemented
+def selecting_key_pp_to_sample(pp: pd.DataFrame, hhid:str, age_col:str, strategy:Literal["oldest", "youngest", "random"]) -> pd.DataFrame:
+    """Filter the pp so each hh only have 1 pp so it can be used for sampling later"""
+    pp = pp.copy(deep=True).reset_index(drop=True)
+    pp["ppid"] = pp.index
+    pp["converted_age"] = pp[age_col].apply(lambda x: int(x.split("-")[0].replace("+", "")))
+    gb_hhid = pp.groupby(hhid)[["ppid", "converted_age"]].apply(lambda x: list(x.to_numpy()))
+    gb_hhid = gb_hhid.apply(lambda x: sorted(x, key=lambda x: x[1]))
+    if strategy == "oldest":
+        gb_hhid = gb_hhid.apply(lambda x: x[-1][0])
+    elif strategy == "youngest":
+        gb_hhid = gb_hhid.apply(lambda x: x[0][0])
+    elif strategy == "random":
+        gb_hhid = gb_hhid.apply(lambda x: x[np.random.randint(0, len(x))][0])
+    else:
+        raise ValueError("Strategy not found")
+    return pp[pp["ppid"].isin(gb_hhid)].drop(columns=["ppid", "converted_age"])
 
-    def run():
-        NotImplemented
+
+def CSP_run(syn_hh: pd.DataFrame, pools_pp: dict, pp_atts: list, hh_atts: list, all_rela: list) -> Tuple[pd.DataFrame, pd.DataFrame, Dict[str, pd.DataFrame]]:
+    """Run the CSP for the given synthetic households and pools"""
+
+    syn_results = {HH_TAG: syn_hh}
+    removed_recs = {}
+    
+    for layer in ordered_pairs:
+        for root_rela, sample_rela in layer:
+            print(f"Processing {root_rela} - {sample_rela}")
+            pool_name = f"{root_rela}-{sample_rela}"
+            sample_cols = [f"{x}_{sample_rela}" for x in pp_atts]
+
+            if root_rela == HH_TAG:
+                to_process_syn = syn_hh
+                sample_cols = sample_cols + all_rela
+                evidence_cols = hh_atts
+            else:
+                to_process_syn = syn_results[root_rela]
+                to_process_syn.loc[:, sample_rela] = list(to_process_syn[sample_rela].astype(int))
+                to_process_syn = to_process_syn[to_process_syn[sample_rela] > 0]
+                to_process_syn = create_count_col(to_process_syn, sample_rela)
+                evidence_cols = [f"{x}_{root_rela}" for x in pp_atts]
+            
+            # process to_process_syn if duplicated hhid
+            if to_process_syn[HHID].duplicated().any():
+                to_process_syn = selecting_key_pp_to_sample(to_process_syn, HHID, f"age_{root_rela}", "random")
+
+            rela_pp, removed_syn, _ = sample_matching_from_pairs(
+                given_syn=to_process_syn,
+                syn_id=HHID,
+                paired_pool=pools_pp[pool_name],
+                evidence_cols=evidence_cols,
+                sample_cols=sample_cols,
+            )
+
+            # Update removed syn
+            if root_rela in removed_recs:
+                removed_recs[root_rela].append(removed_syn)
+            else:
+                removed_recs[root_rela] = [removed_syn]
+            
+            # speical process for second layer
+            if sample_rela == "Parent":
+                map_dict = dict(zip(to_process_syn[HHID], to_process_syn["Grandparent"]))
+                rela_pp["Grandparent"] = rela_pp[HHID].map(map_dict)
+            elif sample_rela == "Child":
+                map_dict = dict(zip(to_process_syn[HHID], to_process_syn["Grandchild"]))
+                rela_pp["Grandchild"] = rela_pp[HHID].map(map_dict)
+
+            rela_pp["relationship"] = sample_rela
+            syn_results[sample_rela] = rela_pp
+            rm_hhid = list(removed_syn[HHID])
+            syn_results[root_rela] = syn_results[root_rela][~syn_results[root_rela][HHID].isin(rm_hhid)]
+    
+    print("Concatenating results and output")
+    # We need to concat them
+    temp_pp = []
+    for rela, df in syn_results.items():
+        if rela != HH_TAG:
+            rename_rela = {f"{x}_{rela}": x for x in pp_atts}
+            temp_pp.append(df.rename(columns=rename_rela).drop(columns=all_rela, errors="ignore"))
+    final_pp = pd.concat(temp_pp, ignore_index=True)
+    
+    # return impossible combinations by values
+    for rela, removed in removed_recs.items():
+        removed_recs[rela] = pd.concat(removed, ignore_index=True)
+    final_hh = syn_results[HH_TAG]
+    final_hh.loc[:, HHID] = list(final_hh[HHID].astype(str))
+    final_hh = final_hh[final_hh[HHID].isin(list(final_pp[HHID]))]
+
+    return final_hh, final_pp, removed_recs
+
+
--- PopSynthesis/Methods/IPSF/CSP/run_by_layers.py
+++ PopSynthesis/Methods/IPSF/CSP/run_by_layers.py
@@ -4,129 +4,153 @@
 import pickle
 import pandas as pd
 from PopSynthesis.Methods.IPSF.const import (
+    data_dir,
     processed_dir,
     output_dir,
     PP_ATTS,
-    HH_TAG,
     NOT_INCLUDED_IN_BN_LEARN,
     zone_field,
 )
 from PopSynthesis.Methods.IPSF.CSP.operations.sample_from_pairs import (
-    sample_matching_from_pairs,
-    create_count_col,
     update_by_rm_for_pool,
-    update_by_rm_for_all_pools,
 )
-import numpy as np
-from typing import Literal
+from PopSynthesis.Methods.IPSF.utils.synthetic_checked_census import (
+    adjust_kept_rec_match_census,
+    get_diff_marg,
+    convert_full_to_marg_count,
+)
+from PopSynthesis.Methods.IPSF.SAA.SAA import SAA
+from PopSynthesis.Methods.IPSF.CSP.CSP import CSP_run, HHID
+from typing import Tuple, Dict, List
+import time
 
 
-ordered_pairs =[
-    [
-        ("HH", "Main")
-    ],
-    [
-        ("Main", "Spouse"), 
-        ("Main", "Child"), 
-        ("Main", "Parent"),
-        ("Main", "Sibling"),
-        ("Main", "Others")
-    ], 
-    [
-        ("Child", "Grandchild"), 
-        ("Parent", "Grandparent")
-    ]
-]
+def get_all_data() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, Dict[str, pd.DataFrame]]:
+    # Get stored data
+    print("Loading data")
+    syn_hh = pd.read_csv(
+        output_dir / "SAA_HH_fixed_ad.csv", index_col=0
+    ).reset_index(drop=True)
+    syn_hh[HHID] = syn_hh.index
+
+    hh_pool = pd.read_csv(processed_dir / "HH_pool.csv")
+
+    hh_marg = pd.read_csv(data_dir / "hh_marginals_ipu.csv", header=[0, 1])
+    hh_marg = hh_marg.drop(
+        columns=hh_marg.columns[hh_marg.columns.get_level_values(0) == "sample_geog"][0]
+    )
+
+    with open(processed_dir / "dict_pool_pairs_by_layers.pickle", "rb") as handle:
+        pools_ref = pickle.load(handle)
+
+    return syn_hh, hh_pool, hh_marg, pools_ref
 
 
-def selecting_key_pp_to_sample(pp: pd.DataFrame, hhid:str, age_col:str, strategy:Literal["oldest", "youngest", "random"]) -> pd.DataFrame:
-    """Filter the pp so each hh only have 1 pp so it can be used for sampling later"""
-    pp = pp.copy(deep=True).reset_index(drop=True)
-    pp["ppid"] = pp.index
-    pp["converted_age"] = pp[age_col].apply(lambda x: int(x.split("-")[0].replace("+", "")))
-    gb_hhid = pp.groupby(hhid)[["ppid", "converted_age"]].apply(lambda x: list(x.to_numpy()))
-    gb_hhid = gb_hhid.apply(lambda x: sorted(x, key=lambda x: x[1]))
-    if strategy == "oldest":
-        gb_hhid = gb_hhid.apply(lambda x: x[-1][0])
-    elif strategy == "youngest":
-        gb_hhid = gb_hhid.apply(lambda x: x[0][0])
-    elif strategy == "random":
-        gb_hhid = gb_hhid.apply(lambda x: x[np.random.randint(0, len(x))][0])
-    else:
-        raise ValueError("Strategy not found")
-    return pp[pp["ppid"].isin(gb_hhid)].drop(columns=["ppid", "converted_age"])
+def get_remaining_hh_n_new_marg(hh_marg: pd.DataFrame, final_syn_pop: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    # get the marg from the kept hh
+    marg_from_kept_hh = convert_full_to_marg_count(final_syn_pop, [zone_field])
+    # get the marg from the original marg
+    converted_hh_marg = hh_marg.set_index(
+        hh_marg.columns[hh_marg.columns.get_level_values(0) == zone_field][0]
+    )
+    # get the diff
+    diff_marg = get_diff_marg(converted_hh_marg, marg_from_kept_hh)
+    # adjust the kept hh
+    kept_hh = adjust_kept_rec_match_census(final_syn_pop, diff_marg)
+    # checking
+    kept_marg = convert_full_to_marg_count(kept_hh, [zone_field])
+    new_diff_marg = get_diff_marg(converted_hh_marg, kept_marg)
+    # check it is no neg indeed
+    checking_not_neg = new_diff_marg < 0
+    assert checking_not_neg.any(axis=None) == False
+    # now get the new marg
+    new_diff_marg.index = new_diff_marg.index.astype(int)
+    new_diff_marg.index.name = zone_field
+    return kept_hh, new_diff_marg.reset_index()
+
+
+def update_CSP_combined_syn_hhmarg_pools(syn_hh: pd.DataFrame, hh_marg: pd.DataFrame, pools_ref: Dict[str, pd.DataFrame], pp_atts: List[str], hh_atts: List[str], all_rela: List[str]) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, Dict[str, pd.DataFrame]]:
+    syn_hh, syn_pp, error_recs = CSP_run(syn_hh, pools_ref, pp_atts, hh_atts, all_rela)
+    updated_hh, updated_hh_marg = get_remaining_hh_n_new_marg(hh_marg, syn_hh)
+    updated_pp = syn_pp[syn_pp[HHID].isin(updated_hh[HHID])]
+    updated_pool_ref = {}
+    # update pools by error recs
+    for rela, error_rec in error_recs.items():
+        check_cols = hh_atts if rela == "HH" else [f"{x}_{rela}" for x in pp_atts]
+        updated_pool_ref[rela] = update_by_rm_for_pool(error_rec, pools_ref[rela], check_cols)
+
+    return updated_hh, updated_pp, updated_hh_marg, updated_pool_ref
 
 
 def main():
     # TODO: is there anyway to use pp marg
-    HHID = "hhid"
-    main_rela = "Main"
-
-    syn_hh = pd.read_csv(
-        output_dir / "SAA_output_HH_again.csv", index_col=0
-    ).reset_index(drop=True)
-    syn_hh["hhid"] = syn_hh.index
-
-    with open(processed_dir / "dict_pool_pairs_by_layers.pickle", "rb") as handle:
-        pools_ref = pickle.load(handle)
-    hh_pool = pd.read_csv(processed_dir / "HH_pool.csv")
+    syn_hh, hh_pool, hh_marg, pools_ref = get_all_data()
     
     # get attributes
     pp_atts = list(set(PP_ATTS) - set(NOT_INCLUDED_IN_BN_LEARN))
     hh_atts = [x for x in syn_hh.columns if x not in [zone_field, HHID]]
     all_rela = [x.split("-")[-1] for x in pools_ref.keys()]
 
-    # NOTE: the syn main people will be updated with the new values, it is the first val in the array
-    syn_results = {HH_TAG: syn_hh}
-    # Will do again, will do synthesis by levels to ensure the age order
-    for layer in ordered_pairs:
-        for root_rela, sample_rela in layer:
-            pool_name = f"{root_rela}-{sample_rela}"
-            sample_cols = [f"{x}_{sample_rela}" for x in pp_atts]
+    # Run the CSP - first run
+    updated_syn_hh, syn_pp, hh_marg, pools_ref = update_CSP_combined_syn_hhmarg_pools(syn_hh, hh_marg, pools_ref, pp_atts, hh_atts, all_rela)
 
-            if root_rela == HH_TAG:
-                to_process_syn = syn_hh
-                sample_cols = sample_cols + all_rela
-                evidence_cols = hh_atts
-            else:
-                to_process_syn = syn_results[root_rela]
-                to_process_syn.loc[:, sample_rela] = list(to_process_syn[sample_rela].astype(int))
-                to_process_syn = to_process_syn[to_process_syn[sample_rela] > 0]
-                to_process_syn = create_count_col(to_process_syn, sample_rela)
-                evidence_cols = [f"{x}_{root_rela}" for x in pp_atts]
-            
-            # process to_process_syn if duplicated hhid
-            if to_process_syn[HHID].duplicated().any():
-                to_process_syn = selecting_key_pp_to_sample(to_process_syn, HHID, "age", "random")
+    order_adjustment = [
+        "hhsize",
+        "hhinc",
+        "totalvehs",
+        "dwelltype",
+        "owndwell",
+    ]  # these must exist in both marg and syn
 
-            rela_pp, removed_syn, _ = sample_matching_from_pairs(
-                given_syn=to_process_syn,
-                syn_id=HHID,
-                paired_pool=pools_ref[pool_name],
-                evidence_cols=evidence_cols,
-                sample_cols=sample_cols,
-            )
-            
-            # speical process for second layer
-            if sample_rela == "Parent":
-                map_dict = dict(zip(to_process_syn[HHID], to_process_syn["Grandparent"]))
-                rela_pp["Grandparent"] = rela_pp[HHID].map(map_dict)
-            elif sample_rela == "Child":
-                map_dict = dict(zip(to_process_syn[HHID], to_process_syn["Grandchild"]))
-                rela_pp["Grandchild"] = rela_pp[HHID].map(map_dict)
-                
-            rela_pp["relationship"] = sample_rela
-            syn_results[sample_rela] = rela_pp
-            rm_hhid = list(removed_syn[HHID])
-            syn_results[root_rela] = syn_results[root_rela][~syn_results[root_rela][HHID].isin(rm_hhid)]
-    
-    # We need to concat them
-    temp_pp = []
-    for rela, df in syn_results.items():
-        if rela != HH_TAG:
-            rename_rela = {f"{x}_{rela}": x for x in pp_atts}
-            temp_pp.append(df.rename(columns=rename_rela).drop(columns=all_rela, errors="ignore"))
-    final_pp = pd.concat(temp_pp, ignore_index=True)
+    start_time = time.time()
+    n_run_time = 0
+    # init with the total HH we want
+    n_removed_err_hh = hh_marg.sum().sum() / len(order_adjustment)
+    MAX_RUN_TIME = 30
+    chosen_hhs = [updated_syn_hh]
+    chosen_pp = [syn_pp]
+    err_rm_hh = []
+    highest_id = updated_syn_hh[HHID].astype(int).max()
+    while n_run_time < MAX_RUN_TIME and n_removed_err_hh > 0:
+        # randomly shuffle for each adjustment
+        err_rm_hh.append(n_removed_err_hh)
+        print(
+            f"For run {n_run_time}, order is: {order_adjustment}, aim for {n_removed_err_hh} HHs"
+        )
+        saa = SAA(hh_marg, order_adjustment, order_adjustment, hh_pool)
+        ###
+        added_syn_hh = saa.run(extra_name=f"_IPSF_{n_run_time}")
+        added_syn_hh[HHID] = range(highest_id+1, highest_id+len(added_syn_hh)+1)
+        ###
+        # error check
+        new_syn_hh, new_syn_pp, hh_marg, pools_ref = update_CSP_combined_syn_hhmarg_pools(added_syn_hh, hh_marg, pools_ref, pp_atts, hh_atts, all_rela)
+
+        n_run_time += 1
+        n_removed_err_hh = len(added_syn_hh) - len(new_syn_hh)
+        if n_run_time == MAX_RUN_TIME:
+            # not adjusting anymore
+            chosen_hhs.append(added_syn_hh)
+        else:
+            # continue with adjusting for missing
+            chosen_hhs.append(new_syn_hh)
+        chosen_pp.append(new_syn_pp)
+
+    final_syn_hh = pd.concat(chosen_hhs)
+    final_syn_pp = pd.concat(chosen_pp)
+
+
+    # record time
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    hours, rem = divmod(elapsed_time, 3600)  # 3600 seconds in an hour
+    minutes, seconds = divmod(rem, 60)  # 60 seconds in a minute
+    print(f"IPSF took {int(hours)}h-{int(minutes)}m-{seconds:.2f}s")
+    print(f"Error hh rm are: {err_rm_hh}")
+
+    # output
+    final_syn_hh.to_csv(output_dir / "IPSF_HH.csv")
+    final_syn_pp.to_csv(output_dir / "IPSF_PP.csv")
+   
 
 if __name__ == "__main__":
     main()
