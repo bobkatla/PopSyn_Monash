190d99839d568176f78c171d27427252f1416ccd
Duc Minh La <dminh.bob.la@gmail.com>
2024-11-13 00:24:09 +1100

    minors fixing to handle different cases

--- PopSynthesis/Methods/IPSF/utils/condensed.py
+++ PopSynthesis/Methods/IPSF/utils/condensed.py
@@ -7,17 +7,20 @@ from PopSynthesis.Methods.IPSF.const import count_field
 from typing import Union
 
 
-def condense_df(df: Union[pl.DataFrame, pd.DataFrame], id_col: str) -> pl.DataFrame:
+def condense_df(df: Union[pl.DataFrame, pd.DataFrame], id_col: Union[None, str]= None) -> pl.DataFrame:
     """Convert the disaggregated data into a condensed format."""
-    assert id_col in df.columns
     if isinstance(df, pd.DataFrame):
         df = pl.from_pandas(df)
-    return df.groupby(pl.exclude(id_col)).agg(
-        pl.col(id_col), pl.len().alias(count_field)
-    )
+    expr = pl.all()
+    to_agg = [pl.len().alias(count_field)]
+    if id_col is not None:
+        assert id_col in df.columns
+        expr = pl.exclude(id_col)
+        to_agg.append(pl.col(id_col))
+    return df.group_by(expr).agg(*to_agg)
 
 
-def explode_df(df: pl.DataFrame, id_col: str) -> pl.DataFrame:
+def explode_df_by_id(df: pl.DataFrame, id_col: str) -> pl.DataFrame:
     """Convert the condensed data back to the disaggregated format."""
     assert id_col in df.columns
     assert count_field in df.columns
