74813f73adb91a983c2391299c539477dbca1a54
Duc Minh La <dminh.bob.la@gmail.com>
2025-04-20 09:38:54 +1000

    updated main and const for CSP to run basics

--- PopSynthesis/Methods/CSP/const.py
+++ PopSynthesis/Methods/CSP/const.py
@@ -4,8 +4,9 @@ from pathlib import Path
 DATA_FOLDER = Path(__file__).parent / "data"
 OUTPUT_FOLDER = Path(__file__).parent / "output"
 
-HHID = "hhid"
+HHID = "hhid" # same as in the seed as well as later in the synthesis
 ZONE_ID = "zone_id"
+
 HH_SZ = "hhsize" # we constrainted by this
 HH_ATTS = [
     "dwelltype",
@@ -21,3 +22,6 @@ PP_ATTS = [
     "nolicence",
     "anywork",
 ]
+
+hh_seed_file_path = DATA_FOLDER / "hh_sample_ipu.csv"
+pp_seed_file_path = DATA_FOLDER / "pp_sample_ipu.csv"
--- PopSynthesis/Methods/CSP/main.py
+++ PopSynthesis/Methods/CSP/main.py
@@ -3,17 +3,41 @@
 
 import pandas as pd
 from PopSynthesis.Methods.CSP.run.run import run_csp
+from PopSynthesis.Methods.CSP.const import (
+    DATA_FOLDER,
+    OUTPUT_FOLDER,
+    HHID,
+    ZONE_ID,
+    HH_SZ,
+    HH_ATTS,
+    RELATIONSHIP,
+    PP_ATTS,
+    hh_seed_file_path,
+    pp_seed_file_path,
+)
 
 
 def load_configurations():
     """from const file, create a config dict"""
-    NotImplemented
+    config = {
+        "hh_seed": pd.read_csv(hh_seed_file_path)[HH_ATTS + [HHID, HH_SZ]],
+        "pp_seed": pd.read_csv(pp_seed_file_path)[PP_ATTS + [HHID, RELATIONSHIP]],
+        "hhid": HHID,
+        "zone_id": ZONE_ID,
+        "hh_size": HH_SZ,
+        "relationship": RELATIONSHIP,
+        "output_folder": OUTPUT_FOLDER,
+    }
+    return config
 
 
 def main():
-    hh_df = pd.read_csv()
+    hh_df = pd.read_csv(DATA_FOLDER / "IPF_HH.csv")
     configs = load_configurations()
     # Run CSP with the given hh_df and configs
-
     resulted_pp = run_csp(hh_df, configs) # We must not change the hh
-    print(resulted_pp)
+    # print(resulted_pp)
+
+
+if __name__ == "__main__":
+    main()
