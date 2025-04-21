b6fe7872b60bfffb2d4c6bd1bb2398a43dac192f
Duc Minh La <dminh.bob.la@gmail.com>
2025-04-21 19:03:46 +1000

    updated to have the hhid included and maintained throughout the process

--- PopSynthesis/Methods/CSP/run/csp_sample.py
+++ PopSynthesis/Methods/CSP/run/csp_sample.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from typing import Dict, List, Tuple
 from PopSynthesis.Methods.CSP.run.create_pool_pairs import HH_TAG, MAIN_PERSON, COUNT_COL, EXPECTED_RELATIONSHIPS, EPXECTED_CONNECTIONS
-
+from PopSynthesis.Methods.CSP.const import HHID
 
 TEMP_ID = "temp_id"
 SYN_COUNT_COL = "syn_count"
@@ -25,9 +25,11 @@ def split_and_process_by_temp_id(df: pd.DataFrame, evidence_cols: List[str], tem
 def get_potentials_to_sample(conditionals: pd.DataFrame, evidences: pd.DataFrame, can_sample_all: bool) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """Process conditionals by having it evidence as index and process the remainings"""
     assert COUNT_COL in conditionals.columns, "Count column must be in conditionals"
-    assert set(evidences.columns).issubset(set(conditionals.columns)), "Evidences columns must be subset of conditionals columns"
 
-    evidence_cols = evidences.columns.tolist()
+    evidence_cols = [x for x in evidences.columns if x != HHID]
+
+    assert set(evidence_cols).issubset(set(conditionals.columns)), "Evidences must be subset of conditionals"
+
     conditionals = conditionals.reset_index(drop=True) # ensure we have a clean index
     conditionals[TEMP_ID] = conditionals.index # add a temp id to sample
     evidences_to_ids, evidences_to_counts, sample_cond = split_and_process_by_temp_id(conditionals, evidence_cols, TEMP_ID)
@@ -35,14 +37,16 @@ def get_potentials_to_sample(conditionals: pd.DataFrame, evidences: pd.DataFrame
     assert set(evidences_to_ids.index) == set(evidences_to_counts.index), "Evidences to ids and counts must be same things"
 
     # count the evidences occurences
-    evidences_counts = evidences.value_counts()
+    evidences_hhid = evidences.groupby(evidence_cols)[HHID].apply(list)
+    evidences_counts = evidences[evidence_cols].value_counts()
     if can_sample_all:
         assert set(evidences_counts.index) <= set(evidences_to_ids.index), "Given evidences must be subset of condtionals ids" 
         assert set(evidences_counts.index) <= set(evidences_to_counts.index), "Counts must be subset of condtionals counts"
-    to_sample_df = evidences_counts.reset_index(name=SYN_COUNT_COL)
 
-    to_sample_df[MAP_IDS_COL] = evidences_counts.index.map(evidences_to_ids)
-    to_sample_df[MAP_COUNTS_COL] = evidences_counts.index.map(evidences_to_counts)
+    to_sample_df = evidences_hhid.reset_index(name=HHID)
+    to_sample_df[SYN_COUNT_COL] = evidences_hhid.index.map(evidences_counts)
+    to_sample_df[MAP_IDS_COL] = evidences_hhid.index.map(evidences_to_ids)
+    to_sample_df[MAP_COUNTS_COL] = evidences_hhid.index.map(evidences_to_counts)
 
     # split to_sample_df by NA vals
     possible_to_sample_df = to_sample_df[to_sample_df[MAP_IDS_COL].notna()]
@@ -67,27 +71,35 @@ def direct_sample_from_conditional(conditionals: pd.DataFrame, evidences: pd.Dat
         n = row[SYN_COUNT_COL]
         return np.random.choice(ids, size=n, p=probs, replace=True)
     
-    # Merge to get final chosen results
+    # Apply to get the sampled ids
     to_sample_df = possible_to_sample_df.copy()
     to_sample_df[TEMP_ID] = to_sample_df.apply(sample_by_row, axis=1)
-    to_sample_df = to_sample_df.explode(TEMP_ID).drop(columns=[MAP_IDS_COL, MAP_COUNTS_COL, SYN_COUNT_COL])
+
+    # explode both hhid and chose at same time to get the results
+    to_sample_df['zip_cols'] = to_sample_df.apply(lambda row: list(zip(row[HHID], row[TEMP_ID])), axis=1)
+    to_sample_df = to_sample_df.explode('zip_cols')
+    to_sample_df[[HHID, TEMP_ID]] = pd.DataFrame(to_sample_df['zip_cols'].tolist(), index=to_sample_df.index)
+    to_sample_df = to_sample_df.drop(columns=[MAP_IDS_COL, MAP_COUNTS_COL, SYN_COUNT_COL, 'zip_cols'])
+
+    # merge with known conds to get the final sampled df
     final_sampled_df = to_sample_df.merge(sample_cond, on=TEMP_ID, how="inner").drop(columns=[TEMP_ID])
     assert len(final_sampled_df) == len(evidences), "Sampled df must be same length as original df"
+
     return final_sampled_df
 
 
 def determine_n_rela_for_each_hh(hh_df: pd.DataFrame, hhsz: str, n_rela_conditional: pd.DataFrame) -> Dict[str, int]:
     # we can just build a BN or a direct sample from the hh_df
-    hh_df_rename = hh_df.rename(columns={col: f"{HH_TAG}_{col}" for col in hh_df.columns})
+    hh_df_rename = hh_df.rename(columns={col: f"{HH_TAG}_{col}" for col in hh_df.columns if col != HHID})
     return direct_sample_from_conditional(n_rela_conditional, hh_df_rename, can_sample_all=True)
 
 
 def csp_sample_by_hh(hh_df: pd.DataFrame, final_conditonals: Dict[str, pd.DataFrame], hhsz:str, relationship:str) -> pd.DataFrame:
+    # add hhid
+    if HHID not in hh_df.columns:
+        hh_df[HHID] = hh_df.reset_index(drop=True).index + 1
     processed_hh_df = determine_n_rela_for_each_hh(hh_df, hhsz, final_conditonals[f"{HH_TAG}-counts"])
-    hh_main = None  # this ensure to always have results
-    # Then we slowly sample one by one and remove the unneeded ones
-    # firstly we must have a tree structs
-    # starting to sample layer by layer
-    # loop through layers have the keep track, init from cond (all possible each)
+    assert processed_hh_df[HHID].nunique() == len(hh_df), "Processed hh df must have same hhid as original hh df"
+    print(processed_hh_df)
 
     return None
