49ca428467429e50c6431db8129f04760ea37cd0
Duc Minh La <dminh.bob.la@gmail.com>
2025-04-23 22:53:21 +1000

    working with resampling for impossibles

--- PopSynthesis/Methods/CSP/run/csp_BN_sample.py
+++ PopSynthesis/Methods/CSP/run/csp_BN_sample.py
@@ -13,15 +13,17 @@ from pgmpy.base import DAG
 from PopSynthesis.Methods.CSP.run.rela_const import HH_TAG, RELA_BY_LEVELS, BACK_CONNECTIONS, EXPECTED_RELATIONSHIPS, COUNT_COL
 from PopSynthesis.Methods.CSP.const import HHID, PP_ATTS
 from typing import Dict, List, Callable
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
+n_rela_cols = [f"n_{rela}" for rela in EXPECTED_RELATIONSHIPS]
+CHECK_COL = "check_possible"
 
-def model_sample(model: DAG, evidences: pd.DataFrame, target_cols: List[str], func_check: Callable ) -> pd.DataFrame:
+def model_sample(model: DAG, evidences: pd.DataFrame, target_cols: List[str], func_check: Callable) -> pd.DataFrame:
     # groupby evidences into different combinations and count
     assert HHID in evidences.columns, "The evidences must contain the HHID column"
-    assert COUNT_COL not in evidences.columns, "The given evidences must be full population"
     evidence_cols = [x for x in evidences.columns if x != HHID]
     processed_evidences = evidences.groupby(evidence_cols)[HHID].apply(list).reset_index()
-    processed_evidences[COUNT_COL] = processed_evidences[HHID].apply(len)
 
     # loop through each to sample
     inference = BayesianModelSampling(model)
@@ -29,12 +31,26 @@ def model_sample(model: DAG, evidences: pd.DataFrame, target_cols: List[str], fu
     for _, row in processed_evidences.iterrows():
         # get the evidence for this row
         sample_evidences = [State(att, state) for att, state in row[evidence_cols].items()]
+        comb_correct_results = []
         # sample from the model
-        sampled_results = inference.likelihood_weighted_sample(evidence=sample_evidences, size=row[COUNT_COL])
-        random.shuffle(row[HHID])
-        sampled_results[HHID] = row[HHID]
-        # do the resample impossibles here
-        sampled_dfs.append(sampled_results)
+        hhid_ls = row[HHID]
+        random.shuffle(hhid_ls)
+        while True:
+            sampled_results = inference.likelihood_weighted_sample(evidence=sample_evidences, size=len(hhid_ls))
+            sampled_results[HHID] = hhid_ls
+            if func_check is None:
+                comb_correct_results.append(sampled_results)
+                break
+            check_possible = func_check(sampled_results.copy())
+            correct_samples = sampled_results[check_possible]
+            incorrect_samples = sampled_results[~check_possible]
+            if len(correct_samples) > 0:
+                comb_correct_results.append(correct_samples)
+            if len(incorrect_samples) == 0:
+                break
+            hhid_ls = incorrect_samples[HHID].tolist()
+        final_sampled_df = pd.concat(comb_correct_results, ignore_index=True)
+        sampled_dfs.append(final_sampled_df)
     # concat and return
     sampled_df = pd.concat(sampled_dfs, ignore_index=True)
     # check the sampled df
@@ -45,7 +61,6 @@ def model_sample(model: DAG, evidences: pd.DataFrame, target_cols: List[str], fu
 
 def build_models_for_each_connection(conditional: Dict[str, pd.DataFrame], possible_states: Dict[str, List[str]]) -> Dict[str, DAG]:
     # create the models for each connection using possible states
-    n_rela_cols = [f"n_{rela}" for rela in EXPECTED_RELATIONSHIPS]
     results = {}
     for conn, df in conditional.items():
         # get the states for each att
@@ -66,7 +81,6 @@ def build_models_for_each_connection(conditional: Dict[str, pd.DataFrame], possi
 def sample_rela_BN(hh_df: pd.DataFrame, final_conditonals: pd.DataFrame, hhsz: str, relationship: str, possible_states:Dict[str, List[str]]=None) -> pd.DataFrame:
     # go through each and sample, sample directly from the model
     # for some cases we need to resample the impossible cases
-    n_rela_cols = [f"n_{rela}" for rela in EXPECTED_RELATIONSHIPS]
     hh_df = hh_df.rename(columns={col: f"{HH_TAG}_{col}" for col in hh_df.columns if col != HHID})
     hh_counts_cond = final_conditonals[f"{HH_TAG}-counts"]
     # Special handle for n relas to update the possible states
@@ -79,13 +93,21 @@ def sample_rela_BN(hh_df: pd.DataFrame, final_conditonals: pd.DataFrame, hhsz: s
     # create the models
     conn_models = build_models_for_each_connection(final_conditonals, update_possible_states)
 
-    processed_hh_df = direct_model_sample(conn_models[f"{HH_TAG}-counts"], hh_df, n_rela_cols+hh_df.columns.tolist())
+    processed_hh_df = model_sample(conn_models[f"{HH_TAG}-counts"], hh_df, n_rela_cols+hh_df.columns.tolist(), check_hhsz_mismatch)
     print(processed_hh_df)
     evidences_store = {HH_TAG: processed_hh_df}
     
     raise NotImplementedError("This function is not implemented yet")
 
 ###### here some funcs to check the impossible cases
-def check_hhsz_mismatch(df: pd.DataFrame, hhsz: str) -> bool:
+def check_hhsz_mismatch(syn_hh_df: pd.DataFrame) -> pd.Series:
     """Check if the hh size is mismatch with the df"""
-    NotImplemented
+    hhsz = "HH_hhsize" # TODO: hardcoded here, find another way later
+    assert hhsz in syn_hh_df.columns, "The df must contain the hh size column"
+    syn_hh_df["syn_hhsize"] = syn_hh_df[n_rela_cols].sum(axis=1)
+    def check_hhsz(row):
+        if row[hhsz] == "8+":
+            return row["syn_hhsize"] >= 8
+        else:
+            return int(row[hhsz]) == row["syn_hhsize"]
+    return syn_hh_df.apply(check_hhsz, axis=1)
