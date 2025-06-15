765ff142d8d5f832ec59f753b5d7c4ce8ba8cabb
Duc Minh La <dminh.bob.la@gmail.com>
2025-06-15 16:00:10 +1000

    cleaned and reorganized files, also added some files that need to implements for analysis

---
+++ PopSynthesis/analyse/extract_data/extract_comparisons.py
@@ -0,0 +1,8 @@
+"""Use the .yml to extract the data combined for general processing"""
+
+
+def extract_general_from_resulted_syn():
+    NotImplemented
+
+def extract_saa_runs():
+    NotImplemented
---
+++ PopSynthesis/analyse/utils/process_yaml.py
@@ -0,0 +1,8 @@
+"""Utils for processing the yaml files"""
+
+
+def read_yaml(file_path):
+    NotImplemented
+
+def handle_relative_paths(file_path, parent_path):
+    NotImplemented
