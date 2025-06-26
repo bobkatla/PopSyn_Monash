60de0d06bac0fb8ac9f4fdbb9c90a2aa962dae48
Duc Minh La <dminh.bob.la@gmail.com>
2025-06-26 22:38:10 +1000

    setup base to connect and get the data based on yml

--- PopSynthesis/analyse/extract_data/extract_comparisons.py
+++ PopSynthesis/analyse/extract_data/extract_comparisons.py
@@ -1,8 +1,34 @@
 """Use the .yml to extract the data combined for general processing"""
+from pathlib import Path
+import yaml
+import pandas as pd
+# from PopSynthesis.analyse.utils.process_yaml import handle_yaml_abs_path
 
+def extract_general_from_resulted_syn(yaml_path: Path, output_path: Path, level: str = "hh") -> None:
+    """Extract general data from the result .yml file."""
+    with open(yaml_path, 'r') as file:
+        configs = yaml.safe_load(file)
+    for config_run in configs:
+        # method = config_run["method"]
+        for run in range(config_run["reruns"]):
+            result_path = output_path / config_run["output_name"] / f"reruns_{run}"
+            if level == "hh":
+                syn_pop_path = result_path / f"{config_run['hh_syn_name']}"
+            elif level == "pp":
+                syn_pop_path = result_path / f"{config_run['pp_syn_name']}"
+            else:
+                raise ValueError(f"Unknown level: {level}. Use 'hh' or 'pp'.")
+            syn_pop = pd.read_csv(syn_pop_path)
+            
+            print(syn_pop)
 
-def extract_general_from_resulted_syn():
-    NotImplemented
 
 def extract_saa_runs():
     NotImplemented
+
+if __name__ == "__main__":
+    current_file_path = Path(__file__).resolve()
+    IO_path = current_file_path / "../../../../IO"
+    yaml_path = IO_path / "configs/test.yml"
+    corresponding_output_path = IO_path / "output/runs"
+    extract_general_from_resulted_syn(yaml_path, corresponding_output_path)
--- PopSynthesis/analyse/utils/process_yaml.py
+++ PopSynthesis/analyse/utils/process_yaml.py
@@ -1,8 +1,11 @@
 """Utils for processing the yaml files"""
+import yaml
+from pathlib import Path
+from typing import Dict, Any
 
 
-def read_yaml(file_path):
-    NotImplemented
-
-def handle_relative_paths(file_path, parent_path):
-    NotImplemented
+def handle_yaml_abs_path(yaml_path: Path, target_path: Path) -> Path:
+    """Handle the absolute path of the yaml file."""
+    if not yaml_path.is_absolute():
+        yaml_path = target_path.parent / yaml_path
+    return yaml_path
