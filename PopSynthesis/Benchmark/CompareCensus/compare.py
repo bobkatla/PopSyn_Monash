8b2e85476e905fcbf1cf68eb3aa785683d79b2bf
Duc Minh La <dminh.bob.la@gmail.com>
2025-06-29 15:14:30 +1000

    logic for census RMSE

--- PopSynthesis/Benchmark/CompareCensus/compare.py
+++ PopSynthesis/Benchmark/CompareCensus/compare.py
@@ -1,19 +1,29 @@
 """
 Compare with the census
 """
-
 import pandas as pd
-import math
+import numpy as np
 
 
-def compare_RMS_census(marg_census: pd.Series, marg_syn: pd.Series):
-    ls_census_atts = list(marg_census.index.get_level_values(0).unique())
-    ls_syn_atts = list(marg_syn.index.get_level_values(0).unique())
-    assert ls_census_atts == ls_syn_atts
-    n = marg_census[ls_census_atts[0]].sum()
-    hold = 0
-    for k in marg_census.index:
-        census_val = marg_census[k]
-        syn_val = marg_syn[k] if k in marg_syn.index else 0
-        hold += ((census_val - syn_val) / n) ** 2
-    return math.sqrt(hold)
+def powered2_diff(census_data: pd.DataFrame, syn_data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculate the powered 2 difference between census and synthetic data.
+    """
+    if not set(census_data.columns) == set(syn_data.columns):
+        raise ValueError("Census data columns must match synthetic data columns.")
+    if not set(census_data.index) == set(syn_data.index):
+        raise ValueError("Census data index must match synthetic data index.")
+
+    diff = (census_data - syn_data).pow(2)
+    return diff
+
+
+def get_RMSE(census_data: pd.DataFrame, syn_data: pd.DataFrame, return_type: str = "zonal") -> pd.Series:
+    """
+    Calculate the Root Mean Square Error (RMSE) between census and synthetic data.
+    """
+    diff = powered2_diff(census_data, syn_data)
+    if return_type == "zonal":
+        return diff.mean(axis=1).apply(np.sqrt)
+    elif return_type == "attribute":
+        return diff.mean(axis=0).apply(np.sqrt)
